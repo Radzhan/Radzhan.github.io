a3645abfecaee2e729bc171332d508e9668a328c
Radjan Bayhombaev <rbayhombaev@gmail.com>
2023-04-23 19:53:19 +0600

    initialize project add container add logo add sidebar

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from './containers/Sidebar/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
